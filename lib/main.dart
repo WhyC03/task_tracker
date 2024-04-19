3f2210fb5ca8711a0d924ad66916c429823eff67
Yash Chandra <yash24.chandra2003@gmail.com>
2024-04-20 03:51:16 +0530

    Saving and deleting tasks function completed, added icon and Preloader

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:task_tracker/screens/home_screen.dart';
 
@@ -9,10 +10,19 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      // for animated splash screen
+      home: AnimatedSplashScreen(
+        splash: const Icon(
+          Icons.assignment_sharp,
+          color: Colors.blue,
+          size: 80.0,
+        ),
+        nextScreen: const HomeScreen(),
+        duration: 1500,
+        splashTransition: SplashTransition.fadeTransition,
+      ),
     );
   }
 }
-
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_typing_uninitialized_variables, unnecessary_null_comparison
+// ignore_for_file: prefer_typing_uninitialized_variables, unnecessary_null_comparison, avoid_print, use_build_context_synchronously
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:task_tracker/model/task.dart';
@@ -17,7 +17,9 @@ class _HomeScreenState extends State<HomeScreen> {
   var taskController;
   var descriptionController;
   late List<Task> _tasks;
+  late List<bool> _taskDone;
 
+  // for saving Data
   void saveData() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     Task t = Task.fromString(taskController.text, descriptionController.text);
@@ -30,8 +32,10 @@ class _HomeScreenState extends State<HomeScreen> {
     taskController.text = '';
     descriptionController.text = '';
     Navigator.of(context).pop();
+    getTasks();
   }
 
+  // for getting left tasks after computing a function 
   void getTasks() async {
     _tasks = [];
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -41,6 +45,26 @@ class _HomeScreenState extends State<HomeScreen> {
       _tasks.add(Task.fromMap(json.decode(d)));
     }
     print(_tasks);
+
+    _taskDone = List.generate(_tasks.length, (index) => false);
+    setState(() {});
+  }
+
+  // For updating the List of completed tasks and removing them
+  void updatePendingTasksList() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<Task> pendingList = [];
+    for (var i = 0; i < _tasks.length; i++) {
+      if (!_taskDone[i]) pendingList.add(_tasks[i]);
+    }
+
+    var pendingListEncoded = List.generate(
+      pendingList.length,
+      (i) => json.encode(pendingList[i].getMap()),
+    );
+
+    prefs.setString('task', json.encode(pendingListEncoded));
+    getTasks();
   }
 
   @override
@@ -63,55 +87,75 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
         title: Text(
           'Task Manager',
-          style: GoogleFonts.poppins(color: Colors.white),
+          style: GoogleFonts.montserrat(color: Colors.white),
         ),
         backgroundColor: Colors.blue.shade700,
+        actions: [
+          IconButton(
+            // Save all tasks
+            icon: const Icon(Icons.save),
+            onPressed: () => updatePendingTasksList(),
+            color: Colors.white,
+          ),
+          // Clear all tasks
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () async {
+              SharedPreferences prefs = await SharedPreferences.getInstance();
+              prefs.setString('task', json.encode([]));
+
+              getTasks();
+            },
+            color: Colors.white,
+          ),
+        ],
       ),
       body: (_tasks == null)
           ? Center(
               child: Text(
                 'No Tasks added yet!',
-                style: GoogleFonts.poppins(),
+                style: GoogleFonts.montserrat(),
               ),
             )
-          : Column(
-              children: _tasks
-                  .map((e) => Container(
-                        height: 70,
-                        width: MediaQuery.of(context).size.width,
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 10.0, vertical: 5.0),
-                        padding: const EdgeInsets.only(left: 10.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5.0),
-                          border: Border.all(color: Colors.black, width: 0.5),
-                        ),
-                        alignment: Alignment.centerLeft,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              e.task,
-                              style: GoogleFonts.poppins(
-                                  fontWeight: FontWeight.w500),
-                            ),
-                            Checkbox(
-                              value: false,
-                              onChanged: (bool? newValue) {
-                                setState(() {
-                                  // _throwShotAway = newValue!;
-                                });
-                              },
-                              key: GlobalKey(),
-                            )
-                          ],
-                        ),
-                      ))
-                  .toList(),
-            ),
+          : SingleChildScrollView(
+            child: Column(
+                children: _tasks
+                    .map((e) => Container(
+                          height: 70,
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 10.0, vertical: 5.0),
+                          padding: const EdgeInsets.only(left: 10.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5.0),
+                            border: Border.all(color: Colors.black, width: 0.5),
+                          ),
+                          alignment: Alignment.centerLeft,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                e.task,
+                                style: GoogleFonts.montserrat(
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              Checkbox(
+                                value: _taskDone[_tasks.indexOf(e)],
+                                onChanged: (val) {
+                                  setState(() {
+                                    _taskDone[_tasks.indexOf(e)] = val!;
+                                  });
+                                },
+                                key: GlobalKey(),
+                              )
+                            ],
+                          ),
+                        ))
+                    .toList(),
+              ),
+          ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => showModalBottomSheet(
             context: context,
@@ -126,6 +170,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
+                            // Add Task Text
                             Text(
                               'Add Task',
                               style: GoogleFonts.poppins(
@@ -134,6 +179,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                 fontWeight: FontWeight.w500,
                               ),
                             ),
+                            // Close Button
                             GestureDetector(
                               child: const Icon(Icons.close),
                               onTap: () => Navigator.of(context).pop(),
@@ -141,6 +187,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           ],
                         ),
                         const Divider(thickness: 1.2),
+                        // Task
                         const SizedBox(height: 20),
                         TextField(
                           controller: taskController,
@@ -152,10 +199,11 @@ class _HomeScreenState extends State<HomeScreen> {
                             fillColor: Colors.white,
                             filled: true,
                             hintText: 'Enter Task',
-                            hintStyle: GoogleFonts.poppins(),
+                            hintStyle: GoogleFonts.montserrat(),
                           ),
                         ),
                         const SizedBox(height: 20),
+                        // Description
                         TextField(
                           controller: descriptionController,
                           maxLines: 7,
@@ -174,9 +222,9 @@ class _HomeScreenState extends State<HomeScreen> {
                         Container(
                           padding: const EdgeInsets.symmetric(horizontal: 5.0),
                           width: MediaQuery.of(context).size.width,
-                          // height: 200.0,
                           child: Row(
                             children: [
+                              // Reset task Button
                               SizedBox(
                                 width: (MediaQuery.of(context).size.width / 2) -
                                     20,
@@ -185,12 +233,13 @@ class _HomeScreenState extends State<HomeScreen> {
                                       backgroundColor: Colors.white),
                                   child: Text(
                                     'RESET',
-                                    style: GoogleFonts.poppins(
+                                    style: GoogleFonts.montserrat(
                                         fontWeight: FontWeight.bold),
                                   ),
                                   onPressed: () => taskController.text = '',
                                 ),
                               ),
+                              // Add task Button
                               SizedBox(
                                 width: (MediaQuery.of(context).size.width / 2) -
                                     20,
@@ -199,7 +248,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                       backgroundColor: Colors.blue),
                                   child: Text(
                                     'ADD',
-                                    style: GoogleFonts.poppins(
+                                    style: GoogleFonts.montserrat(
                                         fontWeight: FontWeight.bold),
                                   ),
                                   onPressed: () => saveData(),
@@ -207,7 +256,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               ),
                             ],
                           ),
-                        )
+                        ),
                       ],
                     ),
                   ),
